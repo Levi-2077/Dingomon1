ac8c46898ad5fa400c7ba7279555ae94f0d8aeae
Levi-2077 <itmethejus@gmail.com>
2026-07-31 17:21:03 +0530

    react

---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import button from "./loginbutton";
+
+function App(){
+  //js code
+
+  return(
+    //html code
+    <div>
+    <h2>Welcome to react</h2>
+    <p>Hello this is my first try on react</p>
+
+    <button   />
+    <button   />
+    <button   />
+    <button   />
+    <button   />
+    <button   />
+    <button   />
+    
+    </div>
+  )
+
+
+
+
+}
+
+export default App //function calling
---
+++ React/my-first-app/src/loginbutton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function button(){
+
+    return(
+        <div style={{color:'blue', backgroundColor:'red', border:'none', padding:'10px', borderRadius:'12px'}}>
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default button
